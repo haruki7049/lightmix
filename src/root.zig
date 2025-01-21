d15341aeee0d0dd7b1b446623028728a646c16dc
haruki7049 <tontonkirikiri@gmail.com>
2025-01-21 14:02:06 +0900

    docs(DocsComments): Add a few docs comments

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,5 @@
+//! Lightmix
+
 const std = @import("std");
 const testing = std.testing;
 
@@ -12,11 +14,15 @@ test "wave" {
     _ = Wave.init(.{}).apply().data;
 }
 
+/// Builder
+/// Used to build
+/// Usage: Builder.init(.{})
 pub const Builder = struct {
     wave: Wave = Wave.init(.{}),
 
     const initOption = struct { };
 
+    /// `init` method
     fn init(option: initOption) Builder {
         _ = option;
 
@@ -24,17 +30,23 @@ pub const Builder = struct {
     }
 };
 
+/// Wave
+/// Contains Wave data
+/// Usage: Wave.init(.{}).apply()
 const Wave = struct {
     data: []f32 = &[_]f32{},
 
     const initOption = struct { };
 
+    /// `init` method
     fn init(option: initOption) Wave {
         _ = option;
 
         return Wave{};
     }
 
+    /// apply method
+    /// Used to apply function, such as sine function
     fn apply(self: Wave) Wave {
         return self;
     }
