4cf93e1d21d1d0f7eb95b59145e253229b5b0d62
haruki7049 <tontonkirikiri@gmail.com>
2025-02-05 23:06:43 +0900

    Delete Builder

--- src/root.zig
+++ src/root.zig
@@ -3,32 +3,12 @@
 const std = @import("std");
 const testing = std.testing;
 
-test "builder" {
-    _ = Builder.init(.{});
-}
-
 test "wave" {
     _ = Wave.init(.{});
     _ = Wave.init(.{}).data;
     _ = Wave.init(.{}).apply();
     _ = Wave.init(.{}).apply().data;
 }
-
-/// Builder
-/// Used to build
-/// Usage: Builder.init(.{})
-pub const Builder = struct {
-    wave: Wave = Wave.init(.{}),
-
-    const initOption = struct {};
-
-    /// `init` method
-    fn init(option: initOption) Builder {
-        _ = option;
-
-        return Builder{};
-    }
-};
 
 /// Wave
 /// Contains Wave data
