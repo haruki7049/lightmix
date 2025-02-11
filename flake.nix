8b25151c4b90c41b39908ef68b1ec4e24de7c806
haruki7049 <tontonkirikiri@gmail.com>
2025-02-11 20:58:28 +0900

    build(flakes): Init

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+          overlays = [ inputs.rust-overlay.overlays.default ];
+
+          src = lib.cleanSource ./.;
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+          };
+          lightmix = craneLib.buildPackage {
+            inherit src cargoArtifacts;
+            strictDeps = true;
+
+            doCheck = true;
+          };
+          cargo-clippy = craneLib.cargoClippy {
+            inherit src cargoArtifacts;
+            cargoClippyExtraArgs = "--verbose -- --deny warning";
+          };
+          cargo-doc = craneLib.cargoDoc {
+            inherit src cargoArtifacts;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+            programs.actionlint.enable = true;
+            programs.mdformat.enable = true;
+
+            settings.formatter = {
+              mdformat.excludes = [
+                "CODE_OF_CONDUCT.md"
+                "SUPPORT.md"
+              ];
+            };
+          };
+
+          packages = {
+            inherit lightmix;
+            default = lightmix;
+            doc = cargo-doc;
+          };
+
+          checks = {
+            inherit
+              lightmix
+              cargo-clippy
+              cargo-doc
+              ;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              # Rust
+              rust
+
+              # Nix
+              pkgs.nil
+            ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
+        };
+    };
+}
