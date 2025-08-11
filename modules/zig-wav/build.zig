c0a19d69d26460937f0db11373536efd37298027
haruki7049 <tontonkirikiri@gmail.com>
2025-08-11 19:14:46 +0900

    refactor(zig-wav): Update build.zig

--- modules/zig-wav/build.zig
+++ modules/zig-wav/build.zig
@@ -1,21 +1,33 @@
 const std = @import("std");
-const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Make module available as dependency.
-    _ = b.addModule("wav", .{ .root_source_file = b.path("src/wav.zig") });
+    // Library module declaration
+    const lib_mod = b.addModule("zig_wav", .{
+        .root_source_file = b.path("src/wav.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const test_step = b.step("test", "Run library tests");
-    inline for ([_][]const u8{ "src/sample.zig", "src/wav.zig" }) |test_file| {
-        const t = b.addTest(.{
-            .root_source_file = b.path(test_file),
-            .target = target,
-            .optimize = optimize,
-        });
-        const run_test = b.addRunArtifact(t);
-        test_step.dependOn(&run_test.step);
-    }
+    // Library declaration
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "zig_wav",
+        .root_module = lib_mod,
+    });
+
+    // Library installation
+    b.installArtifact(lib);
+
+    // Unit tests declaration
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // Test step
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
