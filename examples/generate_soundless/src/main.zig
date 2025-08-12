c0de6d10e98f9ee2280d6a6b7eca15ea5b59e851
haruki7049 <tontonkirikiri@gmail.com>
2025-08-13 08:24:38 +0900

    feat(examples): Append generate_soundless

---
+++ examples/generate_soundless/src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const Wave = lightmix.Wave;
+const allocator = std.heap.page_allocator;
+
+pub fn main() !void {
+    const generators = Wave.Generators.init(allocator);
+    const data: []const f32 = try generators.soundless(44100);
+    defer generators.deinit(data);
+
+    const soundless_wave: Wave = try Wave.init(data, allocator, .{
+        .sample_rate = 44100,
+        .channels = 1,
+        .bits = 16,
+    });
+    defer soundless_wave.deinit();
+
+    var file = try std.fs.cwd().createFile("result.wav", .{});
+    defer file.close();
+
+    try soundless_wave.write(file);
+}
