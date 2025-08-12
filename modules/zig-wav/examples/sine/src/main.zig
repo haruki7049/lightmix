9b4db93f60ad517bab00301acaa8d096f55d4afe
haruki7049 <tontonkirikiri@gmail.com>
2025-08-12 09:08:28 +0900

    feat(zig-wav): Update

--- modules/zig-wav/examples/sine/src/main.zig
+++ modules/zig-wav/examples/sine/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const zig_wav = @import("zig_wav");
+const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
     const data: []const u8 = @embedFile("./assets/sine.wav");
@@ -13,16 +14,20 @@ pub fn main() !void {
     std.debug.print("{d}\n", .{wav_decoder.remaining()});
 
     var buf: [64]f32 = undefined;
+    var arraylist = std.ArrayList(f32).init(allocator);
+    defer arraylist.deinit();
+
     while (true) {
         // Read samples as f32. Channels are interleaved.
         const samples_read = try wav_decoder.read(f32, &buf);
 
         // < ------ Do something with samples in buf. ------ >
-        std.debug.print("{d}\n", .{samples_read});
-        std.debug.print("{any}\n", .{buf});
+        try arraylist.appendSlice(&buf);
 
         if (samples_read < buf.len) {
             break;
         }
     }
+
+    std.debug.print("{any}\n", .{arraylist.items});
 }
