0e43e3798e9a341769f6895ee50663984acd1bbf
haruki7049 <tontonkirikiri@gmail.com>
2025-08-13 07:42:56 +0900

    refactor(src/wave.zig): Separate Wave to wave.zig

--- src/root.zig
+++ src/root.zig
@@ -5,102 +5,34 @@ const std = @import("std");
 const zig_wav = @import("zig_wav");
 const testing = std.testing;
 
-pub const Wave = struct {
-    const Self = @This();
+pub const Wave = @import("./wave.zig");
 
-    data: []const f32,
-    allocator: std.mem.Allocator,
+test "Wave" {
+    const allocator = testing.allocator;
 
-    sample_rate: usize,
-    channels: usize,
-    bits: usize,
+    const generator = struct {
+        fn sinewave() [44100]f32 {
+            const sample_rate: f32 = 44100.0;
+            const radins_per_sec: f32 = 440.0 * 2.0 * std.math.pi;
 
-    pub const initOptions = struct {
-        sample_rate: usize,
-        channels: usize,
-        bits: usize,
+            var result: [44100]f32 = undefined;
+            var i: usize = 0;
+
+            while (i < result.len) : (i += 1) {
+                result[i] = 0.5 * std.math.sin(@as(f32, @floatFromInt(i)) * radins_per_sec / sample_rate);
+            }
+
+            return result;
+        }
     };
 
-    pub fn init(data: []const f32, allocator: std.mem.Allocator, options: initOptions) !Self {
-        const owned_data = try allocator.alloc(f32, data.len);
-        @memcpy(owned_data, data);
-
-        return Self{
-            .data = owned_data,
-            .allocator = allocator,
-
-            .sample_rate = options.sample_rate,
-            .channels = options.channels,
-            .bits = options.bits,
-        };
-    }
-
-    /// Free the Wave struct
-    pub fn deinit(self: Self) void {
-        self.allocator.free(self.data);
-    }
-
-    /// Create Wave from binary data
-    /// The data argument can receive a binary data, as @embedFile("./assets/sine.wav")
-    /// Therefore you can use this function as:
-    /// const wave = Wave.from_file_content(@embedFile("./asset/sine.wav"), allocator);
-    pub fn from_file_content(content: []const u8, allocator: std.mem.Allocator) !Self {
-        var stream = std.io.fixedBufferStream(content);
-        var decoder = try zig_wav.decoder(stream.reader());
-
-        var buf: [64]f32 = undefined;
-        var arraylist = std.ArrayList(f32).init(allocator);
-
-        while (true) {
-            // Read samples as f32. Channels are interleaved.
-            const samples_read = try decoder.read(f32, &buf);
-
-            // < ------ Do something with samples in buf. ------ >
-            try arraylist.appendSlice(&buf);
-
-            if (samples_read < buf.len) {
-                break;
-            }
-        }
-
-        const sample_rate: usize = decoder.sampleRate();
-        const channels: usize = decoder.channels();
-        const bits: usize = decoder.bits();
-
-        return Self{
-            .data = try arraylist.toOwnedSlice(),
-            .allocator = allocator,
-
-            .sample_rate = sample_rate,
-            .channels = channels,
-            .bits = bits,
-        };
-    }
-
-    pub fn write(self: Self, file: std.fs.File) !void {
-        var encoder = try zig_wav.encoder(i16, file.writer(), file.seekableStream(), self.sample_rate, self.channels);
-        try encoder.write(f32, self.data);
-        try encoder.finalize();
-    }
-
-    pub fn filter(self: Self, filter_fn: fn (self: Self) anyerror!Self) Self {
-        const result: Self = filter_fn(self) catch |err| {
-            std.debug.print("{any}\n", .{err});
-            @panic("Error happened in filter function...");
-        };
-
-        return result;
-    }
-};
-
-test "init & deinit" {
-    const allocator = testing.allocator;
-    const wave = try Wave.from_file_content(@embedFile("./assets/sine.wav"), allocator);
+    const data: [44100]f32 = generator.sinewave();
+    const wave = try Wave.init(data[0..], allocator, .{
+        .sample_rate = 44100,
+        .channels = 1,
+        .bits = 16,
+    });
     defer wave.deinit();
-
-    try testing.expectEqual(wave.data[0], 0.0);
-    try testing.expectEqual(wave.data[1], 5.0109863e-2);
-    try testing.expectEqual(wave.data[2], 1.0003662e-1);
 
     try testing.expectEqual(wave.sample_rate, 44100);
     try testing.expectEqual(wave.channels, 1);
---
+++ src/wave.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const zig_wav = @import("zig_wav");
+const testing = std.testing;
+
+const Self = @This();
+
+data: []const f32,
+allocator: std.mem.Allocator,
+
+sample_rate: usize,
+channels: usize,
+bits: usize,
+
+pub const initOptions = struct {
+    sample_rate: usize,
+    channels: usize,
+    bits: usize,
+};
+
+pub fn init(data: []const f32, allocator: std.mem.Allocator, options: initOptions) !Self {
+    const owned_data = try allocator.alloc(f32, data.len);
+    @memcpy(owned_data, data);
+
+    return Self{
+        .data = owned_data,
+        .allocator = allocator,
+
+        .sample_rate = options.sample_rate,
+        .channels = options.channels,
+        .bits = options.bits,
+    };
+}
+
+/// Free the Wave struct
+pub fn deinit(self: Self) void {
+    self.allocator.free(self.data);
+}
+
+/// Create Wave from binary data
+/// The data argument can receive a binary data, as @embedFile("./assets/sine.wav")
+/// Therefore you can use this function as:
+/// const wave = Wave.from_file_content(@embedFile("./asset/sine.wav"), allocator);
+pub fn from_file_content(content: []const u8, allocator: std.mem.Allocator) !Self {
+    var stream = std.io.fixedBufferStream(content);
+    var decoder = try zig_wav.decoder(stream.reader());
+
+    var buf: [64]f32 = undefined;
+    var arraylist = std.ArrayList(f32).init(allocator);
+
+    while (true) {
+        // Read samples as f32. Channels are interleaved.
+        const samples_read = try decoder.read(f32, &buf);
+
+        // < ------ Do something with samples in buf. ------ >
+        try arraylist.appendSlice(&buf);
+
+        if (samples_read < buf.len) {
+            break;
+        }
+    }
+
+    const sample_rate: usize = decoder.sampleRate();
+    const channels: usize = decoder.channels();
+    const bits: usize = decoder.bits();
+
+    return Self{
+        .data = try arraylist.toOwnedSlice(),
+        .allocator = allocator,
+
+        .sample_rate = sample_rate,
+        .channels = channels,
+        .bits = bits,
+    };
+}
+
+pub fn write(self: Self, file: std.fs.File) !void {
+    var encoder = try zig_wav.encoder(i16, file.writer(), file.seekableStream(), self.sample_rate, self.channels);
+    try encoder.write(f32, self.data);
+    try encoder.finalize();
+}
+
+pub fn filter(self: Self, filter_fn: fn (self: Self) anyerror!Self) Self {
+    const result: Self = filter_fn(self) catch |err| {
+        std.debug.print("{any}\n", .{err});
+        @panic("Error happened in filter function...");
+    };
+
+    return result;
+}
+
+test "init & deinit" {
+    const allocator = testing.allocator;
+    const wave = try Self.from_file_content(@embedFile("./assets/sine.wav"), allocator);
+    defer wave.deinit();
+
+    try testing.expectEqual(wave.data[0], 0.0);
+    try testing.expectEqual(wave.data[1], 5.0109863e-2);
+    try testing.expectEqual(wave.data[2], 1.0003662e-1);
+
+    try testing.expectEqual(wave.sample_rate, 44100);
+    try testing.expectEqual(wave.channels, 1);
+    try testing.expectEqual(wave.bits, 16);
+}
