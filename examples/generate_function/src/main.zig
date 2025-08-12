e03c9b2201d3119ff06898630d72b62f09baea90
haruki7049 <tontonkirikiri@gmail.com>
2025-08-12 10:26:55 +0900

    feat(examples): Create generate_function

---
+++ examples/generate_function/src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const Wave = lightmix.Wave;
+const allocator = std.heap.page_allocator;
+
+pub fn main() !void {
+    const sinewave: Wave = try Wave.init(@embedFile("./assets/sine.wav"), allocator);
+    defer sinewave.deinit();
+
+    const overtone_wave: Wave = try generate_function(sinewave);
+    defer overtone_wave.deinit();
+
+    var file = try std.fs.cwd().createFile("result.wav", .{});
+    defer file.close();
+
+    try overtone_wave.write(file);
+}
+
+fn generate_function(original_wave: Wave) !Wave {
+    var result = std.ArrayList(f32).init(original_wave.allocator);
+
+    for (original_wave.data) |data| {
+        const new_data = data * 2;
+        try result.append(new_data);
+    }
+
+    return Wave{
+        .data = try result.toOwnedSlice(),
+        .allocator = original_wave.allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+        .bits = original_wave.bits,
+    };
+}
