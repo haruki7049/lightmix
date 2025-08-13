ba2b22418691e67aa0e2a777846bc1296fd9afd5
haruki7049 <tontonkirikiri@gmail.com>
2025-08-13 16:50:31 +0900

    chore(examples): Edit generate_sinewave

--- examples/Wave/generate_sinewave/src/main.zig
+++ examples/Wave/generate_sinewave/src/main.zig
@@ -18,15 +18,18 @@ pub fn main() !void {
     try sinewave.write(file);
 }
 
+const c_5: f32 = 523.251;
+const volume: f32 = 1.0;
+
 fn generate_sinewave_data() [44100]f32 {
     const sample_rate: f32 = 44100.0;
-    const radins_per_sec: f32 = 440.0 * 2.0 * std.math.pi;
+    const radins_per_sec: f32 = c_5 * std.math.pi;
 
     var result: [44100]f32 = undefined;
     var i: usize = 0;
 
     while (i < result.len) : (i += 1) {
-        result[i] = 0.5 * std.math.sin(@as(f32, @floatFromInt(i)) * radins_per_sec / sample_rate);
+        result[i] = std.math.sin(@as(f32, @floatFromInt(i)) * radins_per_sec / sample_rate) * volume;
     }
 
     return result;
