2632f6806a09244dcb4b700c10695cdbd74e909a
haruki7049 <tontonkirikiri@gmail.com>
2025-08-18 19:33:20 +0900

    feat(Synth): Initialize

--- src/root.zig
+++ src/root.zig
@@ -2,10 +2,12 @@
 //!
 //! `lightmix` is an audio processing library written by Zig-lang.
 
+pub const Synth = @import("./synth.zig");
 pub const Wave = @import("./wave.zig");
 pub const Composer = @import("./composer.zig");
 
 test "Import tests" {
+    _ = @import("./synth.zig");
     _ = @import("./wave.zig");
     _ = @import("./composer.zig");
 }
