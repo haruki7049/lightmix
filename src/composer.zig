aeae33ea325881c4b79bd57eadab94d689d264d4
haruki7049 <tontonkirikiri@gmail.com>
2025-08-19 18:34:05 +0900

    Update

--- src/composer.zig
+++ src/composer.zig
@@ -6,7 +6,31 @@ const Wave = @import("./root.zig").Wave;
 
 const Self = @This();
 
-info: []const Wave,
+const WaveInfo = struct {
+    wave: Wave,
+    start_point: usize,
+
+    fn to_wave(self: WaveInfo, allocator: std.mem.Allocator) Wave {
+        var padding_data: []f32 = allocator.alloc(f32, self.start_point) catch @panic("Out of memory");
+
+        for (0..padding_data.len) |i| {
+            padding_data[i] = 0.0;
+        }
+
+        const slices: []const []const f32 = &[_][]const f32{ padding_data, self.wave.data };
+        const data = std.mem.concat(allocator, f32, slices);
+
+        const result: Wave = Wave.init(data, allocator, .{
+            .sample_rate = self.wave.sample_rate,
+            .channels = self.wave.channels,
+            .bits = self.wave.bits,
+        });
+
+        return result;
+    }
+};
+
+info: []const WaveInfo,
 allocator: std.mem.Allocator,
 
 sample_rate: usize,
@@ -22,7 +46,7 @@ pub const initOptions = struct {
 pub fn init(allocator: std.mem.Allocator, options: initOptions) Self {
     return Self{
         .allocator = allocator,
-        .info = &[_]Wave{},
+        .info = &[_]WaveInfo{},
 
         .sample_rate = options.sample_rate,
         .channels = options.channels,
@@ -34,8 +58,8 @@ pub fn deinit(self: Self) void {
     self.allocator.free(self.info);
 }
 
-pub fn init_with(info: []const Wave, allocator: std.mem.Allocator, options: initOptions) Self {
-    var list = std.ArrayList(Wave).init(allocator);
+pub fn init_with(info: []const WaveInfo, allocator: std.mem.Allocator, options: initOptions) Self {
+    var list = std.ArrayList(WaveInfo).init(allocator);
     list.appendSlice(info) catch @panic("Out of memory");
 
     return Self{
@@ -48,8 +72,8 @@ pub fn init_with(info: []const Wave, allocator: std.mem.Allocator, options: init
     };
 }
 
-pub fn append(self: Self, wave: Wave) !Self {
-    var d = std.ArrayList(Wave).init(self.allocator);
+pub fn append(self: Self, wave: WaveInfo) !Self {
+    var d = std.ArrayList(WaveInfo).init(self.allocator);
     try d.appendSlice(self.info);
 
     try d.append(wave);
@@ -64,8 +88,8 @@ pub fn append(self: Self, wave: Wave) !Self {
     };
 }
 
-pub fn appendSlice(self: Self, append_list: []const Wave) !Self {
-    var d = std.ArrayList(Wave).init(self.allocator);
+pub fn appendSlice(self: Self, append_list: []const WaveInfo) !Self {
+    var d = std.ArrayList(WaveInfo).init(self.allocator);
     try d.appendSlice(self.info);
 
     try d.appendSlice(append_list);
@@ -81,20 +105,129 @@ pub fn appendSlice(self: Self, append_list: []const Wave) !Self {
 }
 
 pub fn finalize(self: Self) !Wave {
-    var d = std.ArrayList(f32).init(self.allocator);
+    var end_point: usize = 0;
 
-    for (self.info) |wave| {
-        try d.appendSlice(wave.info);
+    // Calculate the length for emitted wave
+    for (self.info) |waveinfo| {
+        const ep = waveinfo.start_point + waveinfo.wave.data.len;
+
+        if (end_point < ep)
+            end_point = ep;
     }
 
-    return Wave{
-        .info = try d.toOwnedSlice(),
-        .allocator = self.allocator,
+    var padded_waveinfo_list = std.ArrayList(WaveInfo).init(self.allocator);
+    defer padded_waveinfo_list.deinit();
 
+    // Filter each WaveInfo to append padding both of start and last
+    for (self.info) |waveinfo| {
+        const padded_at_start: []const f32 = padding_for_start(waveinfo.wave.data, waveinfo.start_point, self.allocator);
+        defer self.allocator.free(padded_at_start);
+
+        const padded_at_start_and_last: []const f32 = padding_for_last(padded_at_start, end_point, self.allocator);
+        defer self.allocator.free(padded_at_start_and_last);
+
+        const wave: Wave = Wave.init(padded_at_start_and_last, self.allocator, .{
+            .sample_rate = self.sample_rate,
+            .channels = self.channels,
+            .bits = self.bits,
+        });
+
+        const wi: WaveInfo = WaveInfo{
+            .wave = wave,
+            .start_point = waveinfo.start_point,
+        };
+
+        try padded_waveinfo_list.append(wi);
+    }
+
+    const padded_waveinfo_slice: []const WaveInfo = try padded_waveinfo_list.toOwnedSlice();
+    defer self.allocator.free(padded_waveinfo_slice);
+
+    const empty_data: []const f32 = generate_soundless_data(end_point, self.allocator);
+    defer self.allocator.free(empty_data);
+
+    var result: Wave = Wave.init(empty_data, self.allocator, .{
         .sample_rate = self.sample_rate,
         .channels = self.channels,
         .bits = self.bits,
-    };
+    });
+
+    for (padded_waveinfo_slice) |waveinfo| {
+        std.debug.print("{d}\n", .{waveinfo.wave.data.len});
+        std.debug.print("{d}\n", .{result.data.len});
+
+        const wave = try result.mix(waveinfo.wave);
+        result.deinit();
+        waveinfo.wave.deinit();
+        result = wave;
+    }
+
+    return result;
+}
+
+fn padding_for_start(data: []const f32, start_point: usize, allocator: std.mem.Allocator) []const f32 {
+    const padding_length: usize = start_point;
+    var padding = std.ArrayList(f32).init(allocator);
+    defer padding.deinit();
+
+    // Append padding
+    for (0..padding_length) |_|
+        padding.append(0.0) catch @panic("Out of memory");
+
+    // Append data slice
+    padding.appendSlice(data) catch @panic("Out of memory");
+
+    const result: []const f32 = padding.toOwnedSlice() catch @panic("Out of memory");
+
+    return result;
+}
+
+fn padding_for_last(data: []const f32, end_point: usize, allocator: std.mem.Allocator) []const f32 {
+    std.debug.assert(data.len <= end_point);
+
+    const padding_length: usize = end_point - data.len;
+    var padding = std.ArrayList(f32).init(allocator);
+    defer padding.deinit();
+
+    // Append data slice
+    padding.appendSlice(data) catch @panic("Out of memory");
+
+    // Append padding
+    for (0..padding_length) |_|
+        padding.append(0.0) catch @panic("Out of memory");
+
+    const result: []const f32 = padding.toOwnedSlice() catch @panic("Out of memory");
+
+    return result;
+}
+
+fn generate_soundless_data(length: usize, allocator: std.mem.Allocator) []const f32 {
+    var list = std.ArrayList(f32).init(allocator);
+    defer list.deinit();
+
+    // Append empty wave
+    for (0..length) |_|
+        list.append(0.0) catch @panic("Out of memory");
+
+    const result: []const f32 = list.toOwnedSlice() catch @panic("Out of memory");
+
+    return result;
+}
+
+test "padding_for_start" {
+    const allocator = testing.allocator;
+    const data: []const f32 = &[_]f32{ 1.0, 1.0 };
+    const start_point: usize = 10;
+
+    const result: []const f32 = padding_for_start(data, start_point, allocator);
+    defer allocator.free(result);
+
+    try testing.expectEqual(data.len + start_point, result.len);
+
+    const expected: []const f32 = &[_]f32{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0 };
+    for (0..result.len) |i| {
+        try testing.expectApproxEqAbs(expected[i], result[i], 0.001);
+    }
 }
 
 test "init & deinit" {
@@ -113,7 +246,7 @@ test "init_with & deinit" {
     const wave = try Wave.from_file_content(@embedFile("./assets/sine.wav"), allocator);
     defer wave.deinit();
 
-    const info: []const Wave = &[_]Wave{ wave, wave };
+    const info: []const WaveInfo = &[_]WaveInfo{ .{ .wave = wave, .start_point = 0 }, .{ .wave = wave, .start_point = 0 } };
 
     const composer = Self.init_with(info, allocator, .{
         .sample_rate = 44100,
@@ -135,10 +268,10 @@ test "append" {
     const wave = try Wave.from_file_content(@embedFile("./assets/sine.wav"), allocator);
     defer wave.deinit();
 
-    const appended_composer = try composer.append(wave);
+    const appended_composer = try composer.append(.{ .wave = wave, .start_point = 0 });
     defer appended_composer.deinit();
 
-    try testing.expectEqualSlices(Wave, appended_composer.info, &[_]Wave{ wave });
+    try testing.expectEqualSlices(WaveInfo, appended_composer.info, &[_]WaveInfo{ .{ .wave = wave, .start_point = 0 } });
 }
 
 test "appendSlice" {
@@ -153,15 +286,15 @@ test "appendSlice" {
     const wave = try Wave.from_file_content(@embedFile("./assets/sine.wav"), allocator);
     defer wave.deinit();
 
-    var append_list = std.ArrayList(Wave).init(allocator);
+    var append_list = std.ArrayList(WaveInfo).init(allocator);
     defer append_list.deinit();
-    try append_list.append(wave);
-    try append_list.append(wave);
+    try append_list.append(.{ .wave = wave, .start_point = 0 });
+    try append_list.append(.{ .wave = wave, .start_point = 0 });
 
     const appended_composer = try composer.appendSlice(append_list.items);
     defer appended_composer.deinit();
 
-    try testing.expectEqualSlices(Wave, appended_composer.info, &[_]Wave{ wave, wave });
+    try testing.expectEqualSlices(WaveInfo, appended_composer.info, &[_]WaveInfo{ .{ .wave = wave, .start_point = 0 }, .{ .wave = wave, .start_point = 0 } });
 }
 
 test "finalize" {
@@ -173,24 +306,24 @@ test "finalize" {
     });
     defer composer.deinit();
 
-    var info: []f32 = try allocator.alloc(f32, 44100);
-    defer allocator.free(info);
+    var data: []f32 = try allocator.alloc(f32, 44100);
+    defer allocator.free(data);
 
-    for (0 .. info.len) |i| {
-        info[i] = 0.0;
+    for (0 .. data.len) |i| {
+        data[i] = 1.0;
     }
 
-    const wave = Wave.init(info, allocator, .{
+    const wave = Wave.init(data, allocator, .{
         .sample_rate = 44100,
         .channels = 1,
         .bits = 16,
     });
     defer wave.deinit();
 
-    var append_list = std.ArrayList(Wave).init(allocator);
+    var append_list = std.ArrayList(WaveInfo).init(allocator);
     defer append_list.deinit();
-    try append_list.append(wave);
-    try append_list.append(wave);
+    try append_list.append(.{ .wave = wave, .start_point = 0 });
+    try append_list.append(.{ .wave = wave, .start_point = 44100 });
 
     const appended_composer = try composer.appendSlice(append_list.items);
     defer appended_composer.deinit();
@@ -198,7 +331,7 @@ test "finalize" {
     const result: Wave = try appended_composer.finalize();
     defer result.deinit();
 
-    try testing.expectEqual(result.info.len, 88200);
+    try testing.expectEqual(result.data.len, 88200);
 
     try testing.expectEqual(result.sample_rate, 44100);
     try testing.expectEqual(result.channels, 1);
