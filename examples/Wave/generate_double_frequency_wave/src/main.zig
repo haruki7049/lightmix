a936c1d0d402e116531ed8d7927b151413b75ec2
haruki7049 <tontonkirikiri@gmail.com>
2025-10-04 14:02:43 +0900

    style: nix fmt

--- examples/Wave/generate_double_frequency_wave/src/main.zig
+++ examples/Wave/generate_double_frequency_wave/src/main.zig
@@ -3,7 +3,8 @@ const lightmix = @import("lightmix");
 const Wave = lightmix.Wave;
 const allocator = std.heap.page_allocator;
 
-pub fn main() !void { const data: [44100]f32 = generate_sinewave_data();
+pub fn main() !void {
+    const data: [44100]f32 = generate_sinewave_data();
     const sinewave: Wave = Wave.init(data[0..], allocator, .{
         .sample_rate = 44100,
         .channels = 1,
