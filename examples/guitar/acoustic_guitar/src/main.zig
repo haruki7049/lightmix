9fe1cc7bb888e89a422519a6f5933e512e52eafc
haruki7049 <tontonkirikiri@gmail.com>
2025-12-22 18:44:46 +0900

    Update examples/guitar/acoustic_guitar

--- examples/guitar/acoustic_guitar/src/main.zig
+++ examples/guitar/acoustic_guitar/src/main.zig
@@ -5,7 +5,10 @@ const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
     const data: [44100]f32 = generate_guitar_note();
-    const guitar: Wave = Wave.init(data[0..], allocator, 44100, 1);
+    const guitar: Wave = Wave.init(data[0..], allocator, .{
+        .sample_rate = 44100,
+        .channels = 1,
+    });
     defer guitar.deinit();
 
     var file = try std.fs.cwd().createFile("result.wav", .{});
