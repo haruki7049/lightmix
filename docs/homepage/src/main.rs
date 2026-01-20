9904d0fd3f47229cf3fe336efb760c4ab1455624
haruki7049 <tontonkirikiri@gmail.com>
2026-01-21 00:53:29 +0900

    Update

--- docs/homepage/src/main.rs
+++ docs/homepage/src/main.rs
@@ -22,11 +22,9 @@ enum Route {
 
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
-
     rsx! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
+        header {
+            h1 { a { href: "/", "lightmix" } }
+        }
     }
 }
