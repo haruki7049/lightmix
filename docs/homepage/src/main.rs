2ff7f7251159f53479cf46f2e4b79b4a77af0ade
haruki7049 <tontonkirikiri@gmail.com>
2026-01-20 23:56:48 +0900

    Update main.rs

--- docs/homepage/src/main.rs
+++ docs/homepage/src/main.rs
@@ -1,14 +1,3 @@
-//! The simplest example of a Dioxus app.
-//!
-//! In this example we:
-//! - import a number of important items from the prelude (launch, Element, rsx, div, etc.)
-//! - define a main function that calls the launch function with our app function
-//! - define an app function that returns a div element with the text "Hello, world!"
-//!
-//! The `launch` function is the entry point for all Dioxus apps. It takes a function that returns an Element. This function
-//! calls "launch" on the currently-configured renderer you have. So if the `web` feature is enabled, it will launch a web
-//! app, and if the `desktop` feature is enabled, it will launch a desktop app.
-
 use dioxus::prelude::*;
 
 fn main() {
@@ -16,7 +5,11 @@ fn main() {
 }
 
 fn app() -> Element {
+    let mut count = use_signal(|| 0);
+
     rsx! {
-        div { "Hello, world!" }
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
     }
 }
